a35b184368f397619206e9f1d06357550556451c
LisaVind <LisaVind@users.noreply.github.com>
2024-12-20 09:55:25 +0100

    Hubs finished

--- src/plot_functions.py
+++ src/plot_functions.py
@@ -57,6 +57,7 @@ def plot_two_distributions(df, column_name1, column_name2, title1, title2,
         ax[1].set_yscale('log') 
 
     plt.tight_layout()
+    plt.savefig(f"images/png/combined_histograms{column_name1}.png")
     plt.show()
 
 
@@ -76,33 +77,33 @@ def plot_top10_pagerank(df_filtered_hubs):
     )
 
     # Add labels and title
-    plt.xlabel("Hub Score", fontsize=12)
+    plt.xlabel("PageRank score", fontsize=12)
     plt.ylabel("Article Names", fontsize=12)
-    plt.title("Top 10 Articles by Hub Score", fontsize=14)
+    plt.title("Top 10 Articles by PageRank score", fontsize=14)
     plt.xticks(fontsize=10)
     plt.yticks(fontsize=10)
 
     plt.legend().remove()
     plt.tight_layout()
-    plt.savefig("top_page_rank_articles.png")
+    plt.savefig("images/png/top_page_rank_articles.png")
     plt.show()
 
 
 def get_correlation_matrix_row(df_filtered_hubs, cols):
     df_hubs_corr = df_filtered_hubs[cols].corr()
-    df_hubs_corr.rename(columns={'hub_score': 'Hub score'}, inplace=True)
-    df_hubs_corr.rename(index={'hub_score': 'Hub score', 
+    df_hubs_corr.rename(columns={'hub_score': 'PageRank score'}, inplace=True)
+    df_hubs_corr.rename(index={'hub_score': 'PageRank score', 
                             'source_counts': 'Source count', 'target_counts': 'Target count',
                             'mean_shortest_path': 'Mean shortest path from article','mean_shortest_path_to_article': 'Mean shortest path to article'}, inplace=True)
 
     plt.figure(figsize=(12,4))
-    corr_figure = sns.heatmap(df_hubs_corr[['Hub score']].T,
+    corr_figure = sns.heatmap(df_hubs_corr[['PageRank score']].T,
                 vmin=-1,
                 cmap='coolwarm',
                 annot=True)
 
     plt.xticks(rotation=30, ha='right')
-    plt.title('Correlation between Hub score and other variables')
+    plt.title('Correlation between PageRank score and other variables')
     plt.savefig("corr_figure.png")
 
 def plot_page_score_categories(df_filtered_hubs):
@@ -110,11 +111,11 @@ def plot_page_score_categories(df_filtered_hubs):
 
     # Create matrix for plotting
     matrix = pd.DataFrame({
-        "Hub score": df_filtered_hubs[categories].multiply(df_filtered_hubs["hub_score"], axis=0).sum()})
+        "PageRank score": df_filtered_hubs[categories].multiply(df_filtered_hubs["hub_score"], axis=0).sum()})
     
     plt.figure(figsize=(12, 6))
     sns.heatmap(matrix, annot=True, cmap="Blues", cbar_kws={'label': 'Mean Value'}, annot_kws={'rotation': 45})
-    plt.title('Sum of Hub score Score by Category in percent')
+    plt.title('Sum of PageRank score Score by Category in percent')
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
     plt.savefig("sum_pagerank_hub_score_percent.png")
@@ -125,7 +126,7 @@ def barplot_page_score_by_category(df_filtered_hubs):
 
     # Create matrix for plotting
     matrix = pd.DataFrame({
-        "Hub score": df_filtered_hubs[categories].multiply(df_filtered_hubs["hub_score"], axis=0).sum()}).T
+        "PageRank score": df_filtered_hubs[categories].multiply(df_filtered_hubs["hub_score"], axis=0).sum()}).T
 
     number_of_articles_order = ['Science', 'Geography','People','History','Everyday life','Design and Technology','Countries',
     'Citizenship','Language and literature','Religion','Music','Business Studies','IT','Mathematics',
@@ -134,8 +135,8 @@ def barplot_page_score_by_category(df_filtered_hubs):
     plt.figure(figsize=(12, 6))
     sns.barplot(x=matrix.columns, y=matrix.iloc[0],order=number_of_articles_order, color="#0f4584")
 
-    plt.title('Sum of Hub Score by Category')
-    plt.ylabel('Hub score')
+    plt.title('Sum of PageRank score by Category')
+    plt.ylabel('PageRank score')
     plt.xlabel('')
     plt.xticks(rotation=45, ha='right')
 
@@ -149,7 +150,7 @@ def barplot_page_score_by_category_normalized(df_filtered_hubs):
     number_articles = df_filtered_hubs[categories].sum(axis=0)
     # Create matrix for plotting
     matrix = pd.DataFrame({
-        "Hub score": df_filtered_hubs[categories].multiply(df_filtered_hubs["hub_score"], axis=0).sum()}).T
+        "PageRank score": df_filtered_hubs[categories].multiply(df_filtered_hubs["hub_score"], axis=0).sum()}).T
 
     number_of_articles_order = ['Science', 'Geography','People','History','Everyday life','Design and Technology','Countries',
     'Citizenship','Language and literature','Religion','Music','Business Studies','IT','Mathematics',
@@ -158,8 +159,8 @@ def barplot_page_score_by_category_normalized(df_filtered_hubs):
     plt.figure(figsize=(12, 6))
     sns.barplot(x=matrix.columns, y=matrix.iloc[0]/number_articles.values,order=number_of_articles_order, color="#0f4584")
 
-    plt.title('Sum of Hub Score Score by Category')
-    plt.ylabel('Hub Score normalized by article count')
+    plt.title('Sum of PageRank score Score by Category')
+    plt.ylabel('PageRank score normalized by article count')
     plt.xlabel('')
     plt.xticks(rotation=45, ha='right')
 
